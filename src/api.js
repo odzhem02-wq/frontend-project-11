006943e4cf63358bdcd20ca0c17d8443788e9862
dzhemdzhem <o.dzhem02@mail.ru>
2026-04-11 14:16:34 +0300

    fix api and controller lint

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const buildProxyUrl = (url) => {
-  const proxyUrl = new URL('https://allorigins.hexlet.app/get');
-  proxyUrl.searchParams.set('disableCache', 'true');
-  proxyUrl.searchParams.set('url', url);
-  return proxyUrl.toString();
-};
+const buildProxyUrl = url => {
+  const proxyUrl = new URL('https://allorigins.hexlet.app/get')
+  proxyUrl.searchParams.set('disableCache', 'true')
+  proxyUrl.searchParams.set('url', url)
+  return proxyUrl.toString()
+}
 
-export default (url) => axios.get(buildProxyUrl(url));
+export default url => axios.get(buildProxyUrl(url))
